e0503c13400ebd92d26984ba0382b8b7d71b566c
Dzhengiz <dablekim@outlook.com>
2023-11-06 08:50:02 +0200

    bubbles project creation

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,70 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="mouse-trail-container" @mousemove="addCircle">
+    <CustomCursor v-if="showCursor" />
+    <div
+      v-for="circle in circles"
+      :key="circle.id"
+      class="circle"
+      :style="circle.style"
+    ></div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CustomCursor from "./components/CustomCursor.vue";
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    CustomCursor,
+  },
+  data() {
+    return {
+      circles: [],
+      showCursor: false,
+    };
+  },
+  methods: {
+    addCircle(event) {
+      const { x, y } = event;
+      const delay = this.circles.length * 100;
+
+      this.circles.push({
+        id: Date.now(),
+        style: {
+          left: x + "px",
+          top: y + "px",
+          opacity: 1,
+        },
+      });
+
+      if (this.circles.length > 5) {
+        const removedCircle = this.circles.shift();
+        setTimeout(() => {
+          removedCircle.style.opacity = 0;
+        }, delay);
+      }
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  cursor: none;
+}
+
+.mouse-trail-container {
+  position: relative;
+  width: 100%;
+  height: 100vh;
+}
+
+.circle {
+  position: absolute;
+  width: 20px;
+  height: 20px;
+  border: 2px solid #000;
+  border-radius: 50%;
+  transition: opacity 0.5s ease;
 }
 </style>
---
+++ src/components/CustomCursor.vue
@@ -0,0 +1,43 @@
+<template>
+  <div
+    class="custom-cursor"
+    :style="{ left: `${cursorX}px`, top: `${cursorY}px` }"
+  ></div>
+</template>
+
+<style scoped>
+.custom-cursor {
+  width: 30px;
+  height: 30px;
+  border: 2px solid #000;
+  border-radius: 50%;
+  position: fixed;
+  transform: translate(-50%, -50%);
+  pointer-events: none;
+  z-index: 9999;
+}
+</style>
+
+<script>
+export default {
+  name: "CustomCursor",
+  data() {
+    return {
+      cursorX: -100, // Initial position off-screen
+      cursorY: -100,
+    };
+  },
+  mounted() {
+    window.addEventListener("mousemove", this.updateCursorPosition);
+  },
+  methods: {
+    updateCursorPosition(event) {
+      this.cursorX = event.clientX;
+      this.cursorY = event.clientY;
+    },
+  },
+  beforeDestroy() {
+    window.removeEventListener("mousemove", this.updateCursorPosition);
+  },
+};
+</script>
